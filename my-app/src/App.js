b62c54ac91b7ad39aee18336051d585dbfd513bb
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-03 13:35:03 -0700

    Fetched user and follower data in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
@@ -10,6 +11,55 @@ class App extends React.Component {
       followers: []
     };
   }
+  
+  getUserData = () => {
+    axios 
+      .get('https://api.github.com/users/sara-alkhamri')
+      .then(res => {
+        console.log(res.data);
+        this.setState({...this.state.data, user: res.data})
+      })
+      .catch(err => {
+        console.log('error!')
+      })
+  }
+
+  getFolllowerData = () => {
+    axios
+      .get('https://api.github.com/users/sara-alkhamri/followers')
+      .then(res => {
+        console.log(res.data);
+        this.setState({...this.state.data, followers: res.data})
+      })
+      .catch(err => {
+        console.log('error!')
+      })
+  }
+
+  componentDidMount() {
+    this.getUserData()
+    this.getFolllowerData()
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>Github Card</h1>
+      
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default App;
